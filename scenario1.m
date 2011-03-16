634ff51e93ce60b731a68723e748810f354ae7df
irjudson <irjudson@gmail.com>
2011-03-16 13:10:21 -0600

    Plot scripts

--- scenario1.m
+++ scenario1.m
@@ -11,40 +11,13 @@ SSPkNN = SSPkNN / 1000000;
 plot(X,SSPOPT,'--og ','LineWidth',3);
 hold on;
 plot(X,SSPLPR,':*b','LineWidth',3);
-plot(X,SSPMST,'-..c','LineWidth',3);
+plot(X,SSPMST,'-.sc','LineWidth',3);
 plot(X,SSPkNN,'-+r','LineWidth',3);
 xlabel('Number of Nodes');
 ylabel('Throughput (Mbps)');
 legend('SSP-OPT','SSP-LPR','SSP-MST','SSP-kNN', "Location", "NorthWest")
+set(gca, 'XTickMode', 'manual', 'XTick', X);
 hold off;
-
-function fixAxes
-%---------------------------------------
-%// Kludge to fix scaling of all figures
-%// until GNU or I can find real fix.
-%// Octave3.2.3 computes the scaling wrong
-%// for this mac, such that the title 
-%// and xlabel are not displayed.
-%---------------------------------------
-s = get(0,'showhiddenhandles');
-set(0,'showhiddenhandles','on');
-newpos = [0.15 0.15 0.78 0.78];        %// default is [0.13 0.11 0.775 0.815]
-figs = get(0,'children');
-if (~isempty(figs))
-  for k=1:length(figs)
-	  cax = get(figs(k),'currentaxes');
-pos = get(cax,'position');       
-if ~(pos(1) == newpos(1) && ... 
-     pos(2) == newpos(2) && ...
-     pos(3) == newpos(3) && ...
-     pos(4) == newpos(4))
-set(cax,'position',newpos);    
-set(0,'currentfigure',figs(k));
-drawnow();
-        endif
-    endfor
-endif
-	set(0,'showhiddenhandles',s);
-%---------------------------------------
-endfunction
-%---------------------------------------
+fixAxes;
+print(1, "scenario1.eps", "-deps");
+print(1, "scenario1.pdf", "-dpdf");
--- scenario3.m
+++ scenario3.m
@@ -8,40 +8,13 @@ SSPMST = SSPMST / 1000000;
 SSPkNN = SSPkNN / 1000000;
 plot(X,SSPLPR,':*b','LineWidth',3);
 hold on;
-plot(X,SSPMST,'-..c','LineWidth',3);
+plot(X,SSPMST,'-.sc','LineWidth',3);
 plot(X,SSPkNN,'--og ','LineWidth',3);
 xlabel('Number of Sectors');
 ylabel('Throughput (Mbps)');
 legend('SSP-LPR','SSP-MST','SSP-kNN', "Location", "NorthWest");
+set(gca, 'XTickMode', 'manual', 'XTick', X);
 hold off;
-
-function fixAxes
-%---------------------------------------
-%// Kludge to fix scaling of all figures
-%// until GNU or I can find real fix.
-%// Octave3.2.3 computes the scaling wrong
-%// for this mac, such that the title 
-%// and xlabel are not displayed.
-%---------------------------------------
-s = get(0,'showhiddenhandles');
-set(0,'showhiddenhandles','on');
-newpos = [0.15 0.15 0.8 0.8];        %// default is [0.13 0.11 0.775 0.815]
-figs = get(0,'children');
-if (~isempty(figs))
-  for k=1:length(figs)
-	  cax = get(figs(k),'currentaxes');
-pos = get(cax,'position');       
-if ~(pos(1) == newpos(1) && ... 
-     pos(2) == newpos(2) && ...
-     pos(3) == newpos(3) && ...
-     pos(4) == newpos(4))
-set(cax,'position',newpos);    
-set(0,'currentfigure',figs(k));
-drawnow();
-        endif
-    endfor
-endif
-	set(0,'showhiddenhandles',s);
-%---------------------------------------
-endfunction
-%---------------------------------------
+fixAxes;
+print(3, "scenario3.eps", "-deps");
+print(3, "scenario3.pdf", "-dpdf");
