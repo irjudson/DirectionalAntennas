d891c7cda5cc9cfafe09fde5b6d93e620105e2cf
irjudson <irjudson@gmail.com>
2011-03-15 18:01:32 -0600

    Server changes

--- job_one.sh
+++ job_one.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-#
+#$ -N DirectionalAntennas_with_optimal
+#$ -S /bin/bash
+#$ -o job_one.log
+#$ -j y
+#$ -cwd
 
 # Test comment
 CPLEXPATH="/opt/cplex"
-CPLEXARCH="x86_-64_darwin9_gcc4.0"
+CPLEXARCH="x86-64_darwin9_gcc4.0"
 CPLEXARCH="x86_sles10_4.1"
+CPLEXARCH="x86-64_sles10_4.1"
 JLPATH="./dist/lib:${CPLEXPATH}/cplex/bin/${CPLEXARCH}:${CPLEXPATH}/lib:${CPLEXPATH}/lib/${CPLEXARCH}"
 
-JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx1024m"
+JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx4096m"
 
 CMD="java $JAVA_OPTIONS -jar dist/DirectionalAntennas.jar"
 
@@ -16,15 +21,5 @@ for nodes in 8 10 12 14 16; do
     for count in 1 2 3 4 5 6 7 8 9 10; do
       echo "Running $count with 8 sectors and $nodes nodes, including optimal."
       $CMD -n $nodes -o -r
-    done
-done
-
-# Runs excluding the optimal solution
-for sectors in 4 6 8 10 12; do
-    for nodes in 10 15 20 25 30; do
-	for count in 1 2 3 4 5 6 7 8 9 10; do
-            echo "Running $count with $sectors sectors and $nodes nodes."
-            $CMD -b $sectors -n $nodes -r
-	done
     done
 done
---
+++ test.sh
@@ -0,0 +1,36 @@
+#$ -N DA
+#$ -S /bin/bash
+#$ -j y
+#$ -cwd
+
+# Test comment
+CPLEXPATH="/opt/cplex"
+CPLEXARCH="x86-64_darwin9_gcc4.0"
+CPLEXARCH="x86_sles10_4.1"
+CPLEXARCH="x86-64_sles10_4.1"
+JLPATH="./dist/lib:${CPLEXPATH}/cplex/bin/${CPLEXARCH}:${CPLEXPATH}/lib:${CPLEXPATH}/lib/${CPLEXARCH}"
+
+JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx3072m"
+
+CMD="java $JAVA_OPTIONS -jar dist/DirectionalAntennas.jar"
+
+$CMD -n 8 -b 8 -s 15441 -o
+
+# Runs including the optimal solution
+#for nodes in 8 10 12 14 16; do
+#    for count in 1 2 3 4 5 6 7 8 9 10; do
+#      echo "Running $count with 8 sectors and $nodes nodes, including optimal."
+#      $CMD -n $nodes -o -r
+#    done
+#done
+
+# Runs excluding the optimal solution
+#for sectors in 4 6 8 10 12; do
+#    for nodes in 10 15 20 25 30; do
+#       for count in 1 2 3 4 5 6 7 8 9 10; do
+#            echo "Running $count with $sectors sectors and $nodes nodes."
+#            $CMD -b $sectors -n $nodes -r
+#       done
+#    done
+#done
+
