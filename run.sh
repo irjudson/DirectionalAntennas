98275235ed66e5e053e8a09e9719ce6f6df44403
irjudson <irjudson@gmail.com>
2012-03-12 09:01:56 -0600

    Updates

--- run.sh
+++ run.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 #
+#$ -N DA
+#$ -S /bin/bash
+#$ -j y
+#$ -cwd 
+#$ -r y
+#$ -shell yes
+#$ -V
+#
 nodes="$1"
 seed="$2"
 sectors="$3"
 
 # Test comment
+CPLEXPATH="/opt/ibm/ILOG/CPLEX_Studio_Academic123"
 CPLEXPATH="/opt/cplex"
 CPLEXARCH="x86_sles10_4.1"
 CPLEXARCH="x86-64_sles10_4.1"
 CPLEXARCH="x86-64_darwin9_gcc4.0"
-JLPATH="./dist/lib:${CPLEXPATH}/cplex/bin/${CPLEXARCH}:${CPLEXPATH}/lib:${CPLEXPATH}/lib/${CPLEXARCH}"
+JLPATH="${CPLEXPATH}/cplex/bin/${CPLEXARCH}:${CPLEXPATH}/lib:${CPLEXPATH}/lib/${CPLEXARCH}"
 
 JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx4096m"
 JAVA_OPTIONS="-Djava.library.path=${JLPATH} -Xmx2048m"
